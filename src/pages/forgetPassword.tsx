75b7273924585a1b6df376ccb017ec171d40b350
Devex Hub <developmentexpert121@gmail.com>
2024-05-02 11:17:20 +0530

    Fixed Folder Structure

--- src/pages/forgetPassword.tsx
+++
@@ -1,9 +0,0 @@
-import ForgetPassword from "../components/Login/ForgetPassword/forgetPassword"
-
-const ForgetPasswordPage = () => {
-  return (
-    <ForgetPassword />
-  )
-}
-
-export default ForgetPasswordPage
--- src/pages/loginPage.tsx
+++
@@ -1,8 +0,0 @@
-import LoginComponent from "../components/Login/login"
-const LoginPage = () => {
-  return (
-    <LoginComponent />
-  )
-}
-
-export default LoginPage
