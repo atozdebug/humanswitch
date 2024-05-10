3a24a64e7f74661a46cc75ef55db53862ce116d8
Devex Hub <developmentexpert121@gmail.com>
2024-05-10 16:28:56 +0530

    Improved

--- src/pages/DashBoard/myReports.tsx
+++ src/pages/DashBoard/myReports.tsx
@@ -90,20 +90,22 @@ const MyReports = () => {
   const [openQuestion, setOpenQuestion] = useState(false);
   const [question, setQuestion] = useState("");
   const [selectedQuestionType, setSelectedQuestionType] = useState<any>({});
-
   const [sliderValues, setSliderValues] = useState<any>({});
   const [options, setOptions] = useState(initialOptions);
   const [nextId, setNextId] = useState(3);
-
   const [questions, setQuestions] = useState<any[]>([]);
-
   const [questionIdCounter, setQuestionIdCounter] = useState(1);
-
   const [answers, setAnswers] = useState<any>({});
+  const [minValues, setMinValues] = useState<any>({});
+  const [maxValues, setMaxValues] = useState<any>({});
+  const [steps, setSteps] = useState<any>({});
+  const [minValueErrors, setMinValueErrors] = useState<any>({});
+  const [maxValueErrors, setMaxValueErrors] = useState<any>({});
 
-  console.log(questions);
+  // console.log("--------------", questions);
+  // console.log(questions);
 
-  console.log(options);
+  // console.log(options);
 
   const handleCloseQuestion = () => {
     setOpenQuestion(false);
@@ -120,15 +122,6 @@ const MyReports = () => {
   const handleSliderChange = (questionId: number, newValue: number) => {
     console.log("-------------", newValue, questionId);
     // Update the slider value for the specified question
-    setQuestions((prevQuestions) => {
-      return prevQuestions.map((question) => {
-        if (question.id === questionId && question.type === "Slider") {
-          // Update the value of the slider for the matching question
-          return { ...question, value: newValue };
-        }
-        return question;
-      });
-    });
     setSliderValues((prevSliderValues: any) => ({
       ...prevSliderValues,
       [questionId]: newValue,
@@ -151,20 +144,27 @@ const MyReports = () => {
         options: initialOptions,
       }),
       ...(selectedQuestionType.name === "Slider" && {
-        value: sliderValues[questionIdCounter] || 8,
-      }),
-      ...(selectedQuestionType.name === "Short Answer" && {
-        answer: "",
+        min: 8,
+        max: 14,
+        steps: 1,
       }),
     };
+
+    setMaxValues((prevMaxValues: any) => ({
+      ...prevMaxValues,
+      [questionIdCounter]: 14,
+    }));
+
+    setMinValues((prevMinValues: any) => ({
+      ...prevMinValues,
+      [questionIdCounter]: 8,
+    }));
 
     // Add the new question to the list of questions
     setQuestions((prevQuestions) => [...prevQuestions, newQuestion]);
 
-    // Increment the question ID counter
     setQuestionIdCounter((prevId) => prevId + 1);
 
-    // Close the dialog and reset state variables
     setOpenQuestion(false);
     setOpen(false);
     setQuestion("");
@@ -184,6 +184,7 @@ const MyReports = () => {
     const updatedOptions = options.filter((option) => option.id !== optionId);
     setOptions(updatedOptions);
   };
+
   const handleRemoveQuestion = (questionId: number) => {
     // Remove the question with the specified ID from the list
     const updatedQuestions = questions.filter(
@@ -193,19 +194,84 @@ const MyReports = () => {
   };
 
   const onChangeTextArea = (questionId: any, newValue: any) => {
-    setQuestions((prevQuestions) => {
-      return prevQuestions.map((question) => {
-        if (question.id === questionId && question.type === "Short Answer") {
-          return { ...question, answer: newValue };
-        }
-        return question;
-      });
-    });
     setAnswers((prevAnswers: any) => ({
       ...prevAnswers,
       [questionId]: newValue,
     }));
   };
+
+  const changeMinValue = (questionId: any, minVal: any) => {
+    if (minVal < maxValues[questionId]) {
+      const minValue = parseInt(minVal, 10);
+      setQuestions((prevQuestions) => {
+        return prevQuestions.map((question) => {
+          if (question.id === questionId && question.type === "Slider") {
+            return { ...question, min: minValue };
+          }
+          return question;
+        });
+      });
+      setMinValues((prevMinValues: any) => ({
+        ...prevMinValues,
+        [questionId]: minValue,
+      }));
+      setMinValueErrors((prevMinValues: any) => ({
+        ...prevMinValues,
+        [questionId]: false,
+      }));
+    } else {
+      setMinValueErrors((prevMinValues: any) => ({
+        ...prevMinValues,
+        [questionId]: true,
+      }));
+    }
+  };
+
+  const changeMaxValue = (questionId: any, maxVal: any) => {
+    if (maxVal > minValues[questionId]) {
+      const maxValue = parseInt(maxVal, 10);
+      setQuestions((prevQuestions) => {
+        return prevQuestions.map((question) => {
+          if (question.id === questionId && question.type === "Slider") {
+            return { ...question, max: maxValue };
+          }
+          return question;
+        });
+      });
+      setMaxValues((prevMaxValues: any) => ({
+        ...prevMaxValues,
+        [questionId]: maxValue,
+      }));
+      setMaxValueErrors((prevMaxValues: any) => ({
+        ...prevMaxValues,
+        [questionId]: false,
+      }));
+    } else {
+      setMaxValueErrors((prevMaxValues: any) => ({
+        ...prevMaxValues,
+        [questionId]: true,
+      }));
+    }
+  };
+
+  const changeStepsValue = (questionId: any, steps: any) => {
+    const stepsValue = parseInt(steps, 10);
+    setQuestions((prevQuestions) => {
+      return prevQuestions.map((question) => {
+        if (question.id === questionId && question.type === "Slider") {
+          return { ...question, steps: stepsValue };
+        }
+        return question;
+      });
+    });
+    setSteps((prevStepsValues: any) => ({
+      ...prevStepsValues,
+      [questionId]: stepsValue,
+    }));
+  };
+
+  console.log(minValueErrors);
+  console.log(maxValueErrors);
 
   return (
     <div className="min-h-screen flex">
@@ -225,121 +291,201 @@ const MyReports = () => {
       <div className="p-6 w-full">
         <Paper sx={{ borderRadius: "16px", py: 4, px: 6 }}>
           <div className="flex flex-col justify-center">
-            {questions.map((question, index) => (
-              <div key={question.id} className="mb-8">
-                <div className="flex justify-between items-center">
-                  <div>
-                    <div className="flex items-center gap-2">
-                      <div className={question.color}>{question.icon}</div>
+            {questions.map((question, index) => {
+              console.log("maxxxxxx", maxValues[question.id]);
+              console.log("minnnnnn", minValues[question.id]);
+              return (
+                <div key={question.id} className="mb-8">
+                  <div className="flex justify-between items-center">
+                    <div>
+                      <div className="flex items-center gap-2">
+                        <div className={question.color}>{question.icon}</div>
 
-                      <div className="text-2xl font-semibold">
-                        Question {index + 1}
+                        <div className="text-2xl font-semibold">
+                          Question {index + 1}
+                        </div>
                       </div>
-                    </div>
-                    <div className="ml-[35px]">{question.text}</div>
-                  </div>
-                  <div>
-                    <Button
-                      size="small"
-                      color="error"
-                      onClick={() => handleRemoveQuestion(question.id)}
-                    >
-                      <DeleteIcon />
-                    </Button>
-                  </div>
-                </div>
-                {question.type === "Short Answer" && (
-                  <div className="mt-2">
-                    <textarea
-                      id={`question-${question.id}`}
-                      name={`question-${question.id}`}
-                      className="bg-white border border-lightblue text-gray-900 text-sm rounded-lg focus:ring-mediumblue focus:border-mediumblue block w-full p-2.5 min-h-62px"
-                      value={answers[question.id] || ""}
-                      onChange={(e) =>
-                        onChangeTextArea(question.id, e.target.value)
-                      }
-                    />
-                  </div>
-                )}
-                {question.type === "Multiple Choice" && (
-                  <div className="mt-2">
-                    <div className="flex">
-                      <div className="mt-2 w-96">
-                        {options.map((option) => (
-                          <div
-                            key={option.id}
-                            className={`flex justify-between items-center content-center border p-4 rounded-xl mb-5`}
-                          >
-                            <div className="flex justify-center items-center gap-4">
-                              <input
-                                id={`default-radio-${option.id}`}
-                                type="radio"
-                                value={option.name}
-                                name={`default-radio`}
-                                className="min-w-[13px] text-blue-600 bg-gray-100  dark:focus:ring-blue-600 dark:ring-offset-gray-800  dark:bg-gray-700"
-                              />
-
-                              <h2 className="font-medium text-sm text-start">
-                                {option.name}
-                              </h2>
-                            </div>
-                            <div>
-                              {options.length > 2 && (
-                                <Button
-                                  onClick={() => handleRemoveOption(option.id)}
-                                  size="small"
-                                  color="error"
-                                >
-                                  <CancelIcon />
-                                </Button>
-                              )}
-                            </div>
-                          </div>
-                        ))}
-                      </div>
+                      <div className="ml-[35px]">{question.text}</div>
                     </div>
                     <div>
-                      {options.length < 4 && (
-                        <Button
-                          sx={{ fontWeight: "bold" }}
-                          onClick={handleAddOption}
-                        >
-                          Add Option
-                        </Button>
-                      )}
+                      <Button
+                        size="small"
+                        color="error"
+                        onClick={() => handleRemoveQuestion(question.id)}
+                      >
+                        <DeleteIcon />
+                      </Button>
                     </div>
                   </div>
-                )}
-                {question.type === "Slider" && (
-                  <Box
-                    sx={{
-                      display: "flex",
-                      gap: 6,
-                      mt: 2,
-                      alignItems: "center",
-                    }}
-                  >
-                    <div style={{ cursor: "pointer", fontWeight: "bold" }}>
-                      {8}
+                  {question.type === "Short Answer" && (
+                    <div className="mt-2 ml-[35px]">
+                      <textarea
+                        id={`question-${question.id}`}
+                        name={`question-${question.id}`}
+                        className="bg-white border border-lightblue text-gray-900 text-sm rounded-lg focus:ring-mediumblue focus:border-mediumblue block w-full p-2.5 min-h-62px"
+                        value={answers[question.id] || ""}
+                        onChange={(e) =>
+                          onChangeTextArea(question.id, e.target.value)
+                        }
+                      />
                     </div>
-                    <Slider
-                      sx={{ width: 250 }}
-                      step={1}
-                      value={sliderValues[question.id] || 8}
-                      valueLabelDisplay="auto"
-                      min={8}
-                      max={14}
-                      onChange={(_, newValue) =>
-                        handleSliderChange(question.id, newValue as number)
-                      }
-                    />
-                    <div style={{ cursor: "pointer", fontWeight: "bold" }}>
-                      {14}
+                  )}
+                  {question.type === "Multiple Choice" && (
+                    <div className="mt-2  ml-[35px]">
+                      <div className="flex">
+                        <div className="mt-2 w-96">
+                          {options.map((option) => (
+                            <div
+                              key={option.id}
+                              className={`flex justify-between items-center content-center border p-4 rounded-xl mb-5`}
+                            >
+                              <div className="flex justify-center items-center gap-4">
+                                <input
+                                  id={`default-radio-${option.id}`}
+                                  type="radio"
+                                  value={option.name}
+                                  name={`default-radio`}
+                                  className="min-w-[13px] text-blue-600 bg-gray-100  dark:focus:ring-blue-600 dark:ring-offset-gray-800  dark:bg-gray-700"
+                                />
+
+                                <h2 className="font-medium text-sm text-start">
+                                  {option.name}
+                                </h2>
+                              </div>
+                              <div>
+                                {options.length > 2 && (
+                                  <Button
+                                    onClick={() =>
+                                      handleRemoveOption(option.id)
+                                    }
+                                    size="small"
+                                    color="error"
+                                  >
+                                    <CancelIcon />
+                                  </Button>
+                                )}
+                              </div>
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                      <div>
+                        {options.length < 4 && (
+                          <Button
+                            sx={{ fontWeight: "bold" }}
+                            onClick={handleAddOption}
+                          >
+                            Add Option
+                          </Button>
+                        )}
+                      </div>
                     </div>
-                  </Box>
-                )}
-              </div>
-            ))}
+                  )}
+                  {question.type === "Slider" && (
+                    <Box
+                      sx={{
+                        display: "flex",
+                        gap: 6,
+                        mt: 2,
+                        alignItems: "center",
+                        ml: "35px",
+                      }}
+                    >
+                      <div
+                        style={{
+                          cursor: "pointer",
+                        }}
+                      >
+                        <div
+                          style={{
+                            cursor: "pointer",
+                            fontWeight: "bold",
+                            display: "flex",
+                            alignItems: "center",
+                            gap: 8,
+                          }}
+                        >
+                          <input
+                            name="minValue"
+                            value={minValues[question.id] || 8}
+                            onChange={(e) =>
+                              changeMinValue(question.id, e.target.value)
+                            }
+                            type="number"
+                            className="w-28 rounded-lg"
+                          />
+                          <div>Min</div>
+                        </div>
+                        {minValueErrors[question.id] && (
+                          <div className="text-xs text-red-600">
+                            Min value cannot be greater than or equal to Max
+                            value
+                          </div>
+                        )}
+                      </div>
+
+                      <Slider
+                        sx={{ width: 250 }}
+                        step={steps[question.id] || 1}
+                        value={sliderValues[question.id] || 8}
+                        valueLabelDisplay="auto"
+                        min={minValues[question.id] || 8}
+                        max={maxValues[question.id] || 14}
+                        onChange={(_, newValue) =>
+                          handleSliderChange(question.id, newValue as number)
+                        }
+                        marks
+                      />
+                      <div
+                        style={{
+                          cursor: "pointer",
+                        }}
+                      >
+                        <div
+                          style={{
+                            cursor: "pointer",
+                            fontWeight: "bold",
+                            display: "flex",
+                            alignItems: "center",
+                            gap: 8,
+                          }}
+                        >
+                          <input
+                            name="maxValue"
+                            value={maxValues[question.id] || 14}
+                            onChange={(e) =>
+                              changeMaxValue(question.id, e.target.value)
+                            }
+                            type="number"
+                            className="w-28 rounded-lg"
+                          />
+                          <div className="flex w-full justify-center">Max</div>
+                        </div>
+                        {maxValueErrors[question.id] && (
+                          <div className="text-xs text-red-600">
+                            Max value cannot be less than or equal to Min value
+                          </div>
+                        )}
+                      </div>
+                      <div className="flex items-center ml-10">
+                        <div className="font-semibold">
+                          Enter Number of steps :
+                        </div>
+                        <input
+                          name="steps"
+                          defaultValue={steps[question.id] || 1}
+                          onChange={(e) =>
+                            changeStepsValue(question.id, e.target.value)
+                          }
+                          type="number"
+                          className="ml-2 w-28 rounded-lg font-bold"
+                        />
+                      </div>
+                    </Box>
+                  )}
+                </div>
+              );
+            })}
             <div className="flex justify-center items-center mt-4">
               <Button variant="contained" onClick={handleClickOpen}>
                 Add Questions
