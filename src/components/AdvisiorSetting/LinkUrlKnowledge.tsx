f9eb3fb5f7c5281773260127cd4155c5a37a5af0
Ajay Boro <ajayboro7277@gmail.com>
2024-08-21 14:55:32 +0530

    fix advisor settings & knowledge base

--- src/components/AdvisiorSetting/LinkUrlKnowledge.tsx
+++ src/components/AdvisiorSetting/LinkUrlKnowledge.tsx
@@ -1,11 +1,11 @@
-import { Button, Drawer } from 'flowbite-react';
-import { useState } from 'react';
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import { Checkbox } from 'flowbite-react';
-import { Dropdown, Flowbite, Pagination, Spinner, Table } from 'flowbite-react';
-import { useDispatch } from 'react-redux';
-import { getFaqs } from '../../services/slices/knowledge_base/faq';
-import { getUrls } from '../../services/slices/knowledge_base/urls';
+import { Button, Drawer } from "flowbite-react";
+import { useState } from "react";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import { Checkbox } from "flowbite-react";
+import { Dropdown, Flowbite, Pagination, Spinner, Table } from "flowbite-react";
+import { useDispatch } from "react-redux";
+import { getFaqs } from "../../services/slices/knowledge_base/faq";
+import { getUrls } from "../../services/slices/knowledge_base/urls";
 export function LinkUrlKnowledge({ allUrls, selectedUrls, setSelectedUrls }) {
   const [isOpen, setIsOpen] = useState(false);
   const dispatch = useDispatch();
@@ -21,7 +21,7 @@ export function LinkUrlKnowledge({ allUrls, selectedUrls, setSelectedUrls }) {
       setSelectedUrls([...selectedUrls, url]);
     } else {
       setSelectedUrls(
-        selectedUrls.filter((selectedUrl) => selectedUrl.id !== url.id)
+        selectedUrls.filter(selectedUrl => selectedUrl.id !== url.id)
       );
     }
   };
@@ -29,62 +29,74 @@ export function LinkUrlKnowledge({ allUrls, selectedUrls, setSelectedUrls }) {
   return (
     <>
       <button
-        type='button'
+        type="button"
         onClick={() => setIsOpen(true)}
-        className='my-2 flex items-center gap-2 border border-span-clr text-span-clr text-sm font-medium px-4 py-[6px] rounded-lg w-fit'
+        className="my-2 flex items-center gap-2 border border-span-clr text-span-clr text-sm font-medium px-4 py-[6px] rounded-lg w-fit"
       >
-        <AddCircleOutlineIcon className='w-[15px]' /> Add Url Knowledge
+        <AddCircleOutlineIcon className="w-[15px]" /> Add Url
       </button>
       <Drawer
         open={isOpen}
         onClose={handleClose}
-        className='max-h-[100vh] h-full w-2/5'
-        position='right'
+        className="max-h-[100vh] h-full w-2/5"
+        position="right"
       >
-        <p>
-          <AddCircleOutlineIcon className='w-[15px]' /> Add Url Knowledge
-        </p>
-        <Drawer.Items>
-          <Table className='!static cursor-pointer '>
-            <Table.Body className='divide-y'>
-              {allUrls &&
-                allUrls?.data?.map((url, index) => {
-                  const isChecked = selectedUrls.some(
-                    (selectedUrl) => selectedUrl.id === url.id
-                  );
+        <div className="flex items-center gap-2">
+          <AddCircleOutlineIcon className="w-[15px]" />
+          <p>Add Url</p>
+        </div>
+        <Drawer.Items className="h-full">
+          {allUrls?.data?.length > 0 ? (
+            <>
+              <Table className="!static cursor-pointer ">
+                <Table.Body className="divide-y">
+                  {allUrls?.data?.map((url, index) => {
+                    const isChecked = selectedUrls.some(
+                      selectedUrl => selectedUrl.id === url.id
+                    );
 
-                  return (
-                    <Table.Row
-                      className='bg-white dark:border-gray-700 dark:bg-gray-800'
-                      key={url?.id}
-                    >
-                      <Table.Cell className='whitespace-nowrap font-medium text-main-heading text-sm dark:text-black'>
-                        <div className='flex items-center gap-3'>
-                          <Checkbox
-                            onChange={(e) => handleMultipleChecked(e, url)}
-                            checked={isChecked}
-                            disabled={!url.is_trained}
-                            className='bg-white outline-none focus:ring-transparent cursor-pointer'
-                          />
-                          {url?.title}
-                        </div>
-                      </Table.Cell>
+                    return (
+                      <Table.Row
+                        className="bg-white dark:border-gray-700 dark:bg-gray-800"
+                        key={url?.id}
+                      >
+                        <Table.Cell className="font-medium text-main-heading text-sm dark:text-black">
+                          <div className="flex items-center gap-3">
+                            <Checkbox
+                              onChange={e => handleMultipleChecked(e, url)}
+                              checked={isChecked}
+                              disabled={!url.is_trained}
+                              className="bg-white outline-none focus:ring-transparent cursor-pointer"
+                            />
+                            {url?.title}
+                          </div>
+                        </Table.Cell>
 
-                      <Table.Cell className='flex justify-end gap-2'>
-                        {url?.is_trained ? <p></p> : <p>Not Trained</p>}
-                      </Table.Cell>
-                    </Table.Row>
-                  );
-                })}
-            </Table.Body>
-          </Table>
-          <div className='flex justify-center mt-3'>
-            <Pagination
-              currentPage={allUrls?.page || 1}
-              totalPages={allUrls?.total_pages || 1}
-              onPageChange={onPageChange}
-            />
-          </div>
+                        <Table.Cell>
+                          {url?.is_trained ? (
+                            <p>Trained</p>
+                          ) : (
+                            <p>Not Trained</p>
+                          )}
+                        </Table.Cell>
+                      </Table.Row>
+                    );
+                  })}
+                </Table.Body>
+              </Table>
+              <div className="flex justify-center mt-3">
+                <Pagination
+                  currentPage={allUrls?.page || 1}
+                  totalPages={allUrls?.total_pages || 1}
+                  onPageChange={onPageChange}
+                />
+              </div>
+            </>
+          ) : (
+            <div className="flex items-center justify-center h-full">
+              <p className="text-center text-gray-500">No URLs Available</p>
+            </div>
+          )}
         </Drawer.Items>
       </Drawer>
     </>
